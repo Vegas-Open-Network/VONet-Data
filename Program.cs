0e3a4aaff7c7cd1c5637b8231c226730c300f487
Jacob Kukuk <me@jacobkukuk.com>
2025-09-28 02:07:52 -0700

    update program build

--- Program.cs
+++ Program.cs
@@ -1,33 +1,21 @@
-using Microsoft.EntityFrameworkCore;
-using VONetData;
-using Microsoft.AspNetCore.Identity;
-using System.IO;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthorization();
 
 app.MapRazorPages();
-
 
 app.Run();
